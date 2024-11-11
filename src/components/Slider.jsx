4d37fb3a25fc9f0ce48e578df30d78c8cf0d5f39
Hafeezbaig <hafeezbaig3138@gmail.com>
2024-11-12 01:16:07 +0530

    Movies API updated and backdrop path added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import GlobalApi from '../services/GlobalApi'
-
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
 function Slider() {
+    const [movieList, setMovieList]=useState([]);
     useEffect(()=>{
         getTrendingMovies();
     },[])
 
     const getTrendingMovies=()=>{
         GlobalApi.getTrendingVideos().then(resp=>{
-            console.log(resp.data.results)
+            console.log(resp.data.results);
+            setMovieList(resp.data.results)
         })
     }
   return (
-    <div>Slider</div>
+    <div>
+        {movieList.map((item)=>(
+            <img src={IMAGE_BASE_URL+item.backdrop_path} 
+            className='min-w-full h-[310px]'/>
+        ))}
+    </div>
   )
 }
 
