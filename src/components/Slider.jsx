10168adb444784db67791d33655d5c1bfffd2d78
Hafeezbaig <hafeezbaig3138@gmail.com>
2024-11-12 01:35:39 +0530

    Slider Images added and responsive

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -14,10 +14,11 @@ function Slider() {
         })
     }
   return (
-    <div>
+    <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none'>
         {movieList.map((item)=>(
             <img src={IMAGE_BASE_URL+item.backdrop_path} 
-            className='min-w-full h-[310px]'/>
+            className='min-w-full h-[310px] object-cover 
+            object-left-top mr-5 rounded-md'/>
         ))}
     </div>
   )
